6f21368252dd296b4cee41c75ed1d1b459b4b44d
nt4rever <levantanald@gmail.com>
2022-11-18 21:00:29 +0700

    add 100dayscss

--- javascript-basic/readFolder.js
+++ javascript-basic/readFolder.js
@@ -18,7 +18,7 @@ const readFolder = (path, sum) => {
   return sum[0];
 };
 
-console.log("Loop folder with map: " + readFolder(folderPath));
+// console.log("Loop folder with map: " + readFolder(folderPath));
 
 const readFolderFor = (path, sum) => {
   sum = sum || [0];
@@ -38,4 +38,28 @@ const readFolderFor = (path, sum) => {
   return sum[0];
 };
 
-console.log("Loop folder with for: " + readFolderFor(folderPath));
+// console.log("Loop folder with for: " + readFolderFor(folderPath));
+
+const readFolderForWait = async (path, sum) => {
+  const res = await new Promise((resolve) =>
+    setTimeout(async () => {
+      sum = sum || [0];
+      const listFile = fs.readdirSync(path);
+      for (file of listFile) {
+        if (fs.statSync(path + "/" + file).isDirectory()) {
+          await readFolderForWait(path + "/" + file, sum);
+        } else {
+          const value = fs.readFileSync(path + "/" + file, {
+            encoding: "utf8",
+            flag: "r",
+          });
+          sum[0] += Number(value);
+        }
+      }
+      resolve(sum[0]);
+    }, 1000)
+  );
+  return res;
+};
+
+readFolderForWait(folderPath).then((res) => console.log(res));
