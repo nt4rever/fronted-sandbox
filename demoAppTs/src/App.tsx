90825972cf76d53eb00d9c39b6c8b1a6b12b5d1f
nt4rever <levantanald@gmail.com>
2023-02-12 15:27:19 +0700

    add repo demoTS

---
+++ demoAppTs/src/App.tsx
@@ -0,0 +1,11 @@
+import DebounceTest from "./components/Debounce";
+
+function App() {
+  return (
+    <div className="p-10">
+      <DebounceTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ demoAppTs/src/hooks/useClickOutSide.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+const useClickOutSide = (callback?: () => void) => {
+  const nodeRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    function handleClickOutSide(this: Document, ev: MouseEvent) {
+      if (nodeRef.current && nodeRef.current.contains(ev.target as Node)) {
+        callback?.();
+      }
+    }
+    document.addEventListener("click", handleClickOutSide);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutSide);
+    };
+  }, []);
+
+  return {
+    nodeRef,
+  };
+};
+
+export default useClickOutSide;
